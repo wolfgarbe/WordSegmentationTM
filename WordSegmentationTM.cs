e76e627c99cbfdb83dd7512e50905bd28b0c3b67
Wolf Garbe <wolf.garbe@faroo.com>
2018-04-21 17:32:29 +0200

    Update WordSegmentationTM.cs

--- WordSegmentationTM.cs
+++ WordSegmentationTM.cs
@@ -1,5 +1,49 @@
-//MIT License: Copyright (c) 2018 Wolf Garbe
-//https://github.com/wolfgarbe/WordSegmentationTM
+// Copyright (C) 2018 Wolf Garbe
+// Version: 1.0
+// Author: Wolf Garbe wolf.garbe@faroo.com
+// Maintainer: Wolf Garbe wolf.garbe@faroo.com
+// URL: //https://github.com/wolfgarbe/WordSegmentationTM
+// Description: https://towardsdatascience.com/fast-word-segmentation-for-noisy-text-2c2c41f9e8da
+
+// MIT License
+// Copyright (c) 2018 Wolf Garbe
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+// and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+
+//The number of all words in the text corpus from which the frequency dictionary was derived
+//Google Books Ngram data: http://storage.googleapis.com/books/ngrams/books/datasetsv2.html
+//The probability P of a word = count of the word in the corpus / number of all words in the corpus 
+public static long N = 1024908267229L;
+
+//dictionary
+public static Dictionary<string, long> dictionary = new Dictionary<string, long>();
+public static int maximumDictionaryWordLength = 0;
+
+//Read word, word frequency pairs from dictionary file.
+public static bool LoadDictionary(String path)
+{
+    String line;
+    String[] word;
+
+    if (!File.Exists(path)) return false;
+
+    using (StreamReader br = new StreamReader(File.OpenRead(path)))
+    {
+        while ((line = br.ReadLine()) != null)
+        {
+            word = line.Split(null);
+            if (word[0].Length > maximumDictionaryWordLength) maximumDictionaryWordLength = word[0].Length;
+            dictionary[word[0]] = long.Parse(word[1]);
+        }
+    }
+    return true;
+}
+
 /// <summary>Find best word segmentation for input string.</summary>
 /// <param name="input">The string being word segmented.</param>
 /// <param name="maxSegmentationWordLength">The maximum word length that should be considered.</param>	
@@ -48,4 +92,10 @@ public static (string segmentedString, decimal probabilityLogSum) WordSegmentati
 	return compositions[input.Length - 1];
 }
 
-Console.WriteLine(WordSegmentationTM("thequickbrownfoxjumpsoverthelazydog", maximumDictionaryWordLength).segmentedString);
+public void test()
+{
+    if (!LoadDictionary(AppDomain.CurrentDomain.BaseDirectory + "../../../frequency_dictionary_en_82_765.txt"))
+        Console.WriteLine("file not found");
+    else
+        Console.WriteLine(WordSegmentationTM("thequickbrownfoxjumpsoverthelazydog", maximumDictionaryWordLength).segmentedString);
+}
